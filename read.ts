500b427189b4925bbddde964e3485a10951ea7b9
Worldkilas <bakaridi43@gmail.com>
2024-07-08 23:19:16 +0100

    added validation of wallet addresses to the reading process

--- read.ts
+++ read.ts
@@ -1,11 +1,35 @@
 import { clusterApiUrl, Connection, PublicKey, LAMPORTS_PER_SOL } from "@solana/web3.js";
+/*
+    Connection: used for establshing connection to a solana cluster
+    Cluster: The different networks you can use solana(devnet,mainnet,testnet)
+    PublicKey: Used for taking a public key
+*/
 
-const connection= new Connection(clusterApiUrl('devnet'))
-const address = new PublicKey('UjrPReZo5dq19a2LwGfAY5MJc8V2F8gnHSgoDgXP6Y3')
-const balance= await connection.getBalance(address)
-//balance in solana
-const balance_in_sol= balance/LAMPORTS_PER_SOL
-console.log(' Your balance is: ',balance);
-console.log('The balance in sol is: ', balance_in_sol);
-//NOTE: Lamports is the smallest unit of tokens in the solana network
+const suppliedPublicKey= process.argv[2]||  null; //takes args from cmdline and the 2nd index is the provided arg
+if (!suppliedPublicKey) {
+    throw new Error("Provide a public key to check balance of");
+    
+}
+
+
+try {
+    const connection= new Connection(clusterApiUrl('devnet'), "confirmed")
+
+const publicKey= new PublicKey(suppliedPublicKey)
+
+if (!PublicKey.isOnCurve(publicKey.toBuffer( ))) {
+    throw new Error("Invalid wallet address");
+    
+}
+
+const balanceInLamports= await connection.getBalance(publicKey)
+
+const balanceInSOL= balanceInLamports/LAMPORTS_PER_SOL
+
+console.log('Your balance in sol is ', balanceInSOL,' while your balance in lamports is ', balanceInLamports);
+
+} catch (error) {
+    console.error("Error: ",error.message);
+    
+}
 
